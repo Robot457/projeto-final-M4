9e1e3c60dfc50194c05fd4a8b971920ea12754cf
Johnson <johnsonbarbosa@outlook.com>
2024-03-01 22:12:12 -0300

    chore: add function to create genres in the database if they don't exist

--- src/database/prisma-client.js
+++ src/database/prisma-client.js
@@ -2,3 +2,34 @@ import { PrismaClient } from '@prisma/client'
 
 export const prismaClient = new PrismaClient()
 
+export const createGenres = async () => {
+  try {
+    const GENRES = [
+      'Ação',
+      'Aventura',
+      'Comédia',
+      'Drama',
+      'Fantasia',
+      'Ficção científica',
+      'Romance',
+      'Terror',
+    ]
+
+    const genresCount = (await prismaClient.genre.findMany()).length
+
+    if (genresCount > 0) {
+      return
+    }
+
+    for (const genre of GENRES) {
+      await prismaClient.genre.create({
+        data: {
+          name: genre,
+        },
+      })
+    }
+  } catch (error) {
+    console.error(error)
+    prismaClient.$disconnect()
+  }
+}
