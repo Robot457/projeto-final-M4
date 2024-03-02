b5c2a9af4eb2626428fe67edaea443be2c34dd78
Johnson <johnsonbarbosa@outlook.com>
2024-03-01 22:10:31 -0300

    feat: add createGenres function call and express.json middleware

--- src/main.js
+++ src/main.js
@@ -5,19 +5,23 @@ import morgan from 'morgan'
 import cors from 'cors'
 
 import { router } from './routes/router.js'
+import { createGenres } from './database/prisma-client.js'
 
 const app = express()
 
 app.use(morgan('dev'))
 app.use(cors())
+app.use(express.json())
 app.use(router)
 
 const port = process.env.PORT || 7000
 
 if (!port) {
-    throw new Error('Porta não definida')
+  throw new Error('Porta não definida')
 }
 
+createGenres()
+
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`)
+  console.log(`Server is running on http://localhost:${port}`)
 })
